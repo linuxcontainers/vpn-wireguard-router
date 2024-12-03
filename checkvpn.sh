7b761b5069dadbd8ea8be3bd777af1db399c2147
Peter <78224555+peter-lxc@users.noreply.github.com>
2024-12-03 14:32:12 +0000

    Update checkvpn.sh

--- checkvpn.sh
+++ checkvpn.sh
@@ -1,22 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
-ping -c 3 www.google.com
+ping -4 -c4 -I wg0 www.google.com
 
 if [ $? -ge 1 ]; then
-	echo "Did not get a response from google, trying to ping yahoo"
-	
-	ping -c 3 www.yahoo.com
-	
-	if [ $? -ge 1 ]; then
-		echo "Did not get a response from yahoo, trying to ping bing"
+        echo "Did not get a response from google, trying to ping yahoo"
 
-		ping -c www.bing.com
+        ping -4 -c4 -I wg0 www.yahoo.com
 
-		if [ $? -ge 1 ]; then
-			echo "Did not get a response from bing rebooting the machine"
-			sudo reboot
-		fi
-	fi
+        if [ $? -ge 1 ]; then
+                echo "Did not get a response from yahoo, trying to ping bing"
+
+                ping -4 -c4 -I wg0 www.bing.com
+
+                if [ $? -ge 1 ]; then
+                        echo "Did not get a response from bing rebooting the machine"
+                        echo "RESTART VPN"
+#                       sudo reboot
+                fi
+        fi
 fi
 
 exit 0
